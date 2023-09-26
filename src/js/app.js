a7c9dcc4bd3ddad9edb50d1b936111adff889a7b
Tatyana <lioness1741@mail.ru>
2023-09-26 15:26:31 +0500

    ready2

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,13 @@
 export default function destructuringObj(obj) {
-    const result = [];
-    const { special } = obj;
-  
-    special.forEach((item) => {
-      const { id, name, icon, description = 'Описание недоступно'} = item;
-      result.push(
-        {
-          id, name, icon, description,
-        },
-      );
-    });
-    return result;
-  }
+  const result = [];
+  const { special } = obj;
+  special.forEach((item) => {
+    const { id, name, icon, description = 'Описание недоступно' } = item;
+    result.push(
+      {
+      id, name, icon, description,
+      },
+    );
+  });
+  return result;
+}
